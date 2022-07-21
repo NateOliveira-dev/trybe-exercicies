d2c904b3534b6a27a9a879f761b0088b080f87bf
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-07-21 12:17:10 -0300

    exercicio 8 completo

---
+++ 01-fundamentos/04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/exercicio8.js
@@ -0,0 +1,11 @@
+const num1 = 21;
+const num2 = 30;
+const num3 = 10;
+
+let ehImpar = false;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0)) {
+  ehImpar = true;
+};
+
+console.log(ehImpar);
