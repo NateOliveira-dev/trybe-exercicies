2f4683ff728555009ae464f98ab70358a607ec3e
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-07-23 18:44:25 -0300

    exercicio 8 ou 9 completo bonus

---
+++ 01-fundamentos/04-introducao-javascript-e-logica-de-programacao/01b-javascript-primeiro-passos/bonus4.js
@@ -0,0 +1,11 @@
+const num1 = 22;
+const num2 = 42;
+const num3 = 12;
+
+let isOdd = false;
+
+if ((num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0)) {
+    isOdd = true;
+};
+
+console.log(isOdd);
