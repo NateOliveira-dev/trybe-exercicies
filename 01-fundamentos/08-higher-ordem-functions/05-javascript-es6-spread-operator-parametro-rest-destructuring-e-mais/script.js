774bb40aa3fb63cf2a2769186d4ee31f0b370cc4
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-08-25 16:28:18 -0300

    bug git

---
+++ 01-fundamentos/08-higher-ordem-functions/05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -0,0 +1,92 @@
+// 1 - Dado o código abaixo, altere a chamada da função rectangleArea de forma que seja impressa a área dos 3 retângulos (rectangle1, rectangle2 e rectangle3). O código deve retornar em sequência 2, 15 e 54.
+// Dica: use spread operator
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+//   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+});
+
+// 2 - Crie uma função sum que dado um número ilimitado de parâmetros retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6) o número 15 deve ser retornado.
+// Dica: use parâmetro rest.
+
+sum = (...args) => {
+    return args.reduce((acc, param) => acc + param, 0);
+};
+// console.log(sum(4,5,6));
+
+// 3 - Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa (variáveis alex e gunnar), retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+// Dica: use object destructuring.
+
+
+// <!-- name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade -->
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// <!-- <nome> tem <anos de idade> e gosta de <gostos da pessoa> -->
+
+// <!-- Retornos esperados: -->
+// console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+
+
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+  ];
+  //bornIn: nascido em
+  
+ const filterPeople = () => {
+    return people.filter(({nationality, bornIn}) => {
+        return nationality === 'Australian'  && bornIn > 1900 && bornIn <= 2000
+    });
+ };
+
+//  console.log(filterPeople());
+
